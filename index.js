93e70690c0a69d5f646d9504c4881b079cd1b332
V24code <190243@virtualwindow.co.za>
2023-05-29 17:35:45 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,17 +2,17 @@
 regAccount = () => {
     let nameSurname = document.getElementById("nameSurname").value
     let email = document.getElementById("email").value
-    let number = document.getElementById("phoneNumber").value
+    let number = document.getElementById("number").value
     let subject = document.getElementById("subject").value
-    
-}
     let checkbox = document.getElementById("check").checked;
-
+    console.log (checkbox)
 
     if (checkbox){
-        alert("Welcome!" + nameSurname + "Thank you for your interest in" + subject + "! We will contact you via the email provided:" + enterEmail + ", or alternatively on the phone number:" + phoneNumber);
+        alert("Welcome!" + nameSurname + "Thank you for your interest in" + subject + "! We will contact you via the email provided:" + email + ", or alternatively on the phone number:" + number);
     }
     else{
         alert("Well, well, well... why don't you want to sign up for our newsletter... you don't seem to be that interested" + nameSurname + "!  If you want to sign up, you better CHECK that newsletter box!");
-    }
+    }   
+}//end of function
+  
     
